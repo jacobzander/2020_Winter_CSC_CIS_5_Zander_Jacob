48b081e9c08cf1fdeea1f5bcf66956b2e294838b
Jacob Zander <jzander1@student.rccd.edu>
2020-02-06 22:06:54 -0800

    Worked more on Blackjack

--- Projects/Project1/BlackjackV4/main.cpp
+++ Projects/Project1/BlackjackV4/main.cpp
@@ -21,39 +21,48 @@ bool addUserDB(string);
 bool isBlackJack(int, int);
 bool isBlackJack(int, int, int);
 bool isBlackJack(int, int, int, int);
+bool isBust(int, int);
+bool isBust(int, int, int);
 string cinLogin();
 string cinLogin(string);
+
 int main(int argc, char** argv) {
     // Set Random number seed
-    srand(static_cast<unsigned int>(time(0)));
-    
+    srand(static_cast<unsigned int> (time(0)));
+
     // Declare Variables
-    
+
     // Login Variables
     string lgin;
     // Want to use a vector for the cards...
     unsigned short bet, pC1, pC2, pC3, pC4, hC1, hC2, hC3, hC4;
-    
+
     // User Variables
     string user;
-    
+
+    // Bet Variables
+    bool betValid;
+
+    // Game Variables
+    bool isGameOn;
+
     // Welcome Screen
     cout << "Welcome to Blackjack (21)" << endl;
-    
+
     // Login Screen
     // Login Validator
     do {
         cout << "Please Login (if new user type n)" << endl;
         cin >> lgin;
     } while (!isLoginValidated(lgin));
-    
-    if(lgin=="n") {
+
+    if (lgin == "n") {
         // Create new user
         string newUser;
         newUser = cinLogin("Enter your name:");
         // Check if the name is on the list
-            // If name is then cancel
-        if(isUserInDB(newUser)) {
+        // If name is then cancel
+        if (isUserInDB(newUser)) {
             cout << "The user is in the db" << endl; // debug
             return 0;
         } else {
@@ -70,47 +79,47 @@ int main(int argc, char** argv) {
         } else { // Name is not on list
             cout << "Login invalid name not in db" << endl;
             return 0;
-        } 
+        }
     }
-    
+
     // Welcome to the game
-    // Move to top
-    // Implementing money in V2 using arrays
-//    int money;
-//    enum Chip { one, five, tf, fifty, hund };
-    
+    // Implementing money in V2 using enums and chips
+    // For right now we will use straight ints but I
+    // want to create a chip system where you either bet one, 
+    // five, twentyfive, fifty or one hundred.
+
     // Cards
     // I wanna use enums to create suit types and value
+
+
     cout << "Welcome " << user << " to blackjack!" << endl;
-    
-    // Bet Do Loop
-    bool betValid = false;
-    do {
-        cout << "How much you want to bet?" << endl;
-        cin >> bet;
-        if (cin.fail())
-            betValid = false;
-        else if (bet <= 0)
-            betValid = false;
-        else
-            betValid = true;
-    } while (!betValid);
-    
+
     // Hand Do Loop
     do {
-        pC1 = rand()%10+1;
-        pC2 = rand()%10+1;
-        
+        betValid = false;
+        do {
+            cout << "How much you want to bet?" << endl;
+            cin >> bet;
+            if (cin.fail())
+                betValid = false;
+            else if (bet <= 0)
+                betValid = false;
+            else
+                betValid = true;
+        } while (!betValid);
+        pC1 = rand() % 10 + 1;
+        pC2 = rand() % 10 + 1;
+
         cout << "First two cards" << pC1 << " amd " << pC2 << endl;
-        
-        if (isBlackJack(pC1, pC2)) 
-            
-        
-        hC1 = rand()%10+1;
-        hC2 = rand()%10+1;
-        
+
+        if (isBlackJack(pC1, pC2))
+
+
+            hC1 = rand() % 10 + 1;
+        hC2 = rand() % 10 + 1;
+
         cout << "Dealer first two cards" << hC1 << " and " << hC2 << endl;
-        
+
         // Player card loop
         bool isPlayerDone = false;
         while (!isPlayerDone) {
@@ -118,17 +127,38 @@ int main(int argc, char** argv) {
             cout << "Do you wanna hit(h) or stand(s)?" << endl;
             cin >> choice;
             if (choice == 'h') {
-                pC3 = rand()%10+1;
-                
+                // Add card to vector here in project 2 TODO
+                // This will allow for more than one card to hit
+
+                isPlayerDone = true;
+            } else if (choice == 's') {
+                isPlayerDone = true;
             }
         }
-        
-    } while(1);
-    
-    
+
+        // Calculate the winner
+        if (isBlackJack(pC1, pC2)) {
+            cout << "You won you got blackjack!" << endl;
+        } else if (isBlackJack(hC1, hC2)) {
+            cout << "The house got blackjack!" << endl;
+        } else {
+            // Determine who is closer to 21 TODO
+            cout << "Nobody won better luck next time ... adding soon." << endl;
+        }
+
+        cout << "To play again press 1 to exit press any other button to countinue" << endl;
+        int tmp;
+        cin >> tmp;
+        if (tmp == 1) {
+            isGameOn = false;
+        } else {
+            isGameOn = true;
+        }
+    } while (isGameOn);
+
+    cout << "Thanks for playing!" << endl;
     return 0;
 }
-
 
 /*
  Login Functions
@@ -137,7 +167,7 @@ int main(int argc, char** argv) {
 string cinLogin() {
     string input;
     do {
-            cin >> input;
+        cin >> input;
     } while (!isLoginValidated(input));
     return input;
 }
@@ -145,8 +175,8 @@ string cinLogin() {
 string cinLogin(string msg) {
     string input;
     do {
-            cout << msg << endl;
-            cin >> input;
+        cout << msg << endl;
+        cin >> input;
     } while (!isLoginValidated(input));
     return input;
 }
@@ -154,17 +184,17 @@ string cinLogin(string msg) {
 // Implementing the cin in this function
 // It's just figuring out if the user wants to create a new player
 // and validating the inputs for looking up players 
+
 bool isLoginValidated(string login) {
     if (login == "n") {
         return true;
     } else {
         for (char c : login) {
             if (isspace(c)) return false;
-            if(!isalpha(c) && !isspace(c)) return false;
+            if (!isalpha(c) && !isspace(c)) return false;
             return true;
         }
     }
-    
 }
 
 /*
@@ -172,15 +202,16 @@ bool isLoginValidated(string login) {
  */
 
 // Checks to see if user is in DB
+
 bool isUserInDB(string user) {
     ifstream inf;
     inf.open("db.dat");
-    
+
     if (!inf) {
         cout << "Could not access the database. Exiting." << endl;
         return false;
     }
-    
+
     while (inf) {
         string strInput;
         inf >> strInput;
@@ -190,6 +221,7 @@ bool isUserInDB(string user) {
 }
 
 // Adds user to DB
+
 bool addUserDB(string user) {
     ofstream ouf;
     ouf.open("db.dat", ios_base::app);
@@ -197,14 +229,44 @@ bool addUserDB(string user) {
 }
 
 // Find the user and update there score in the database // Second Project
+
 bool updateScore(string user) {
-    
+    throw "updateScore is not implemented yet."; // TODO
 }
 
 /*
 Game Functions
  */
 
+// Checks to see if two or more cards are a blackjack
+// There is currently three overloads for this function
+// But in later versions I want there to be one function
+// That takes in a vector that can determine if the hand
+// is a blackjack
+
+bool isBlackJack(int c1, int c2) {
+    if (c1 == 1 && c2 == 10) return true;
+    else return false;
+}
+
+bool isBlackJack(int c1, int c2, int c3) {
+    int total = 0, total2 = 0;
+    total = c1 + c2 + c3;
+    if (c1 == 1) total2 = 11 + c2 + c3;
+    if (c2 == 1) total2 = c1 + 11 + c3;
+    if (c3 == 1) total2 = c1 + c2 + 11;
+    // If the total2 is not available then just check org total
+    if (total2 == 0) {
+        if (total == 21) {
+            return true;
+        }
+    } else {
+        if (total2 == 21 || total == 21) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 /*
@@ -212,6 +274,7 @@ Money Functions
  */
 
 // I want chips to be an object or maybe array idk...
+
 int chipsFromMoney(int money) {
-    
+    throw "chipsFromMoney is not implemented yet."; // TODO
 }
