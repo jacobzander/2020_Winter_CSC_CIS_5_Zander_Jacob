f69ed526cdc8ee5b2548e8dbceb6b297dcf637c6
Jacob Zander <jzander1@student.rccd.edu>
2020-02-16 01:02:11 -0800

    Added DatabaseManager class

---
+++ Projects/Project2/BlackjackV5/DatabaseManager.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   DatabaseManager.cpp
+ * Author: Jacob Zander
+ * 
+ * Created on February 16, 2020, 12:55 AM
+ */
+
+#include "DatabaseManager.h"
+
+DatabaseManager::DatabaseManager() {
+}
+
+DatabaseManager::DatabaseManager(const DatabaseManager& orig) {
+}
+
+DatabaseManager::~DatabaseManager() {
+}
+
---
+++ Projects/Project2/BlackjackV5/DatabaseManager.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   DatabaseManager.h
+ * Author: jacob
+ *
+ * Created on February 16, 2020, 12:55 AM
+ */
+
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+class DatabaseManager {
+public:
+    DatabaseManager();
+    DatabaseManager(const DatabaseManager& orig);
+    virtual ~DatabaseManager();
+private:
+
+};
+
+#endif /* DATABASEMANAGER_H */
+
